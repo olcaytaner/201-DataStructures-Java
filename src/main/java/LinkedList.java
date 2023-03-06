5bf54b60ca2f6b90bd2d8a22457e25c4dcf49fa8
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-03-06 10:32:27 +0300

    Added LinkedList and Node classes. Made an introduction to the students.

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,9 @@
+public class LinkedList {
+    private Node head;
+    private Node tail;
+
+    public LinkedList(){
+        head = null;
+        tail = null;
+    }
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+}
