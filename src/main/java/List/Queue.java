d7b65aa47dcc655c67aa5366a322657a7f5b9860
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-03-27 10:09:04 +0300

    Added Queue data structure.

---
+++ src/main/java/List/Queue.java
@@ -0,0 +1,36 @@
+package List;
+
+public class Queue {
+
+    protected Node first;
+    protected Node last;
+
+    public Queue() {
+        first = null;
+        last = null;
+    }
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+    public void enqueue(Node newNode) {
+        if (first == null) {
+            first = newNode;
+        } else {
+            last.setNext(newNode);
+        }
+        last = newNode;
+    }
+
+    public Node dequeue(){
+        Node result = first;
+        if (!isEmpty()){
+            first = first.getNext();
+            if (first == null){
+                last = null;
+            }
+        }
+        return result;
+    }
+}
