d874ed53ec5ac9269cf59b062620d9de12f86ed4
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-04-01 12:26:12 +0300

    Added Tree and TreeNode classes.

---
+++ src/main/java/Tree/Tree.java
@@ -0,0 +1,18 @@
+package Tree;
+
+public class Tree {
+
+    private TreeNode root;
+
+    public Tree(){
+        root = null;
+    }
+
+    public TreeNode getRoot(){
+        return root;
+    }
+
+    public void setRoot(TreeNode root){
+        this.root = root;
+    }
+}
---
+++ src/main/java/Tree/TreeNode.java
@@ -0,0 +1,27 @@
+package Tree;
+
+public class TreeNode {
+
+    private TreeNode left;
+    private TreeNode right;
+    private int data;
+
+    public TreeNode(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public TreeNode getLeft(){
+        return left;
+    }
+
+    public TreeNode getRight(){
+        return right;
+    }
+
+    public int getData(){
+        return data;
+    }
+
+}
